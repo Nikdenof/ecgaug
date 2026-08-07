defa1560946edc63b6e7e8875877089249c80cd8
nikdenof <nikdenof@gmail.com>
2026-08-08 00:07:11 +0200

    Added first augmentation. Plus dependencies for correct REPL in linux console.

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import matplotlib.pyplot as plt
+import neurokit2 as nk
+import numpy as np
+import pandas as pd
+
+
 def main():
-    print("Hello from ecgaug!")
+    ecg12 = nk.ecg_simulate(duration=11, method="multileads", sampling_rate=500)
+
+    ecg12[0:5000].plot(subplots=True)
+    plt.show()
+
+    ecg12.shape
+
+    ecg_base = ecg12[0:5000].to_numpy()
+
+    mu, sigma = 0, 0.05
+    noise = np.random.normal(mu, sigma, ecg_base.shape)
+    ecg_augmented = ecg_base + noise
+
+    print(type(ecg_augmented), ecg_augmented.shape)
+    pd.DataFrame(ecg_augmented).plot(subplots=True)
+    plt.show()
+
+    # ecg_tf = ecg_base
+    # ecg_torch = np.swapaxes(ecg_base, 1, 0)  # or transpose
 
 
 if __name__ == "__main__":
